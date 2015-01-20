3f7c529157c431e608aeba794208591f0d128389
Eric Rhodes <e_rhodes@live.com>
2015-01-19 23:12:29 -0500

    proper removal of characters_organization relationship

--- app/controllers/characters_organizations_controller.rb
+++ app/controllers/characters_organizations_controller.rb
@@ -3,9 +3,14 @@ class CharactersOrganizationsController < ApplicationController
 		@organization = Organization.find(params[:organization_id])
 		@character = Character.find(params[:characters_organization][:character_id])
 		@organization.members.create(character_id: @character.id, title: "CFO")
-		redirect_to organizations_url
+		redirect_to @organization
 	end
 
 	def destroy
+		@organization = CharactersOrganization.find(params[:id]).organization
+		@character = CharactersOrganization.find(params[:id]).character
+		@organization.members.find_by(character_id: @character.id).destroy
+
+		redirect_to @organization
 	end
 end
--- app/models/character.rb
+++ app/models/character.rb
@@ -2,4 +2,7 @@ class Character < ActiveRecord::Base
 	has_many :known_powers, class_name: "CharactersPower",
 							foreign_key: "character_id",
 							dependent: :destroy
+	has_many :employers, class_name: "CharactersOrganization",
+							foreign_key: "character_id",
+							dependent: :destroy
 end
